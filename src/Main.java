305de0cb4987e2252db6f9e775e91821a069a0da
Luke <lucas.j.powers@ou.edu>
2020-11-04 16:02:21 -0600

    Initial

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		/**
+		 * I'm going to answer 3 questions for class points today: 
+		 * 
+		 * 	Show how you would use Set to find the unique elements in a List.
+		 *	Show an example of HashMap and a TreeMap, and explain the difference. 
+		 *	Which should you use (HashMap or TreeMap), if you have to guarantee the order of key, value pairs?
+		 */
+
+	}
+
+}
